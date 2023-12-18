9217085ab92b945d9111069d154c39de2c4f1f98
AdoyoClifford <adoyoclifford@gmail.com>
2023-12-18 17:04:06 +0300

    initial commit

--- app/src/main/java/com/adoyo/geminipro/Chat.kt
+++ app/src/main/java/com/adoyo/geminipro/Chat.kt
@@ -7,7 +7,7 @@ import com.google.ai.client.generativeai.GenerativeModel
 suspend fun generateText(prompt: String): String? {
     val generativeModel = GenerativeModel(
         modelName = "gemini-pro",
-        apiKey = "AIzaSyBtf4cWulfZgf9cbiLG9VyWpjhQVU_Kiv4"
+        apiKey = BuildConfig.API_KEY
     )
     val response = generativeModel.generateContent(prompt)
     Log.d("ChatRepositoryImpl", "getPrompt: ${response.text}")
